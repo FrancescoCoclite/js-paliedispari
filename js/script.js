1f0422527acbe454e2fd821c71547611129b1a52
Francesco <francescococlite@gmail.com>
2021-06-07 18:05:46 +0200

    end

--- js/script.js
+++ js/script.js
@@ -1,26 +1,26 @@
 // Pari e Dispari L’utente sceglie pari o dispari e inserisce un numero da 1 a 5. Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione). Sommiamo i due numeri. Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione) Dichiariamo chi ha vinto.
 
-// var parDis = (prompt('Pari o Dispari?')).toLowerCase();
-// var numUtente = parseInt(prompt('Scegliere un numero da 1 a 5'))
+var parDis = (prompt('Pari o Dispari?')).toLowerCase();
+  var numUtente = parseInt(prompt('Scegliere un numero da 1 a 5'))
 
-// console.log(parDis);
-// console.log(numUtente);
+ console.log(parDis);
+  console.log(numUtente);
 
-// var numPc = myRandom(1 , 5)
-// console.log(numPc);
+ var numPc = myRandom(1 , 5)
+  console.log(numPc);
 
-// function myRandom(min, max) {
-//     return Math.floor(Math.random() * (max - min + 1) ) + min;
-// }
+ function myRandom(min, max) {
+          return Math.floor(Math.random() * (max - min + 1) ) + min;
+ }
 
-// function add(num1,num2) {
+ function add(num1,num2) {
     
-//     var risultato = (num1 + num2);
-//     return risultato;
-// }   
+    var risultato = (num1 + num2);
+              return risultato;
+         }   
 
-// var somma = add(numUtente, numPc);
-//  console.log(somma);
+         var somma = add(numUtente, numPc);
+   console.log(somma);
 
 // // function vincitore(x) {
 
@@ -36,26 +36,26 @@
 // // var vinto = vincitore(somma);
 
 
-// function vincitore(x) {
-//     var y = "";
-//     if(x % 2 ==0){
-//         y = "pari";
-//     }
-//     else{
-//         y = "dispari";
-//     }
-//     return y;
-// }
-// var vinto = vincitore(somma);
+function vincitore(x) {
+    var y = "";
+    if(x % 2 ==0){
+        y = "pari";
+    }
+    else{
+        y = "dispari";
+    }
+    return y;
+}
+var vinto = vincitore(somma);
 
 
 
-// if(vinto == parDis){
-//     console.log('utente vince');   
-// }
-// else{
-//     console.log('computer vince');
-// }
+if(vinto == parDis){
+    console.log('utente vince');   
+}
+else{
+     console.log('computer vince');
+}
 
 
 // ESERCIZIO PALINDROMO 
